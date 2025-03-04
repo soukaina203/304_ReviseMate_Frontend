05415862abf432235aff4d6c264057328c9c8df8
samenthalo <ortega.samantha81400@gmail.com>
2025-03-04 17:40:27 +0100

    add header

--- src/app/modules/user/tableau-bord/tableau-bord.component.ts
+++ src/app/modules/user/tableau-bord/tableau-bord.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../header/header.component'; 
 
 @Component({
   selector: 'app-tableau-bord',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent], 
   templateUrl: './tableau-bord.component.html',
-  styleUrl: './tableau-bord.component.scss'
+  styleUrls: ['./tableau-bord.component.scss']
 })
 export class TableauBordComponent {
-
 }
