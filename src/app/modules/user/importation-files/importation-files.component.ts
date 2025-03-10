3823e7dcc3107eb2bd254ab806fc666aae1f09d1
samenthalo <ortega.samantha81400@gmail.com>
2025-03-10 13:40:50 +0100

    import files

--- src/app/modules/user/importation-files/importation-files.component.ts
+++ src/app/modules/user/importation-files/importation-files.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgxFileDropModule } from 'ngx-file-drop';  // Importer NgxFileDropModule ici
 
 @Component({
   selector: 'app-importation-files',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgxFileDropModule],  // Ajouter NgxFileDropModule ici
   templateUrl: './importation-files.component.html',
-  styleUrl: './importation-files.component.scss'
+  styleUrls: ['./importation-files.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ImportationFilesComponent {
-
+  onFileDrop(event: { files: any[] }) {
+    console.log(event.files);
+  }
 }
+
