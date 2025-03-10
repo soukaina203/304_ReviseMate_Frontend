5cece7a2f890212dcb919669e0df290dbd6b9378
samenthalo <ortega.samantha81400@gmail.com>
2025-03-11 00:00:08 +0100

    ajout editeur fiche

--- src/app/modules/user/fiches/create-fiche/create-fiche.component.ts
+++ src/app/modules/user/fiches/create-fiche/create-fiche.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { QuillModule } from 'ngx-quill';
+import { FormsModule } from '@angular/forms'; 
+import html2pdf from 'html2pdf.js';
 
 @Component({
   selector: 'app-create-fiche',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, QuillModule, FormsModule],
   templateUrl: './create-fiche.component.html',
-  styleUrl: './create-fiche.component.scss'
+  styleUrls: ['./create-fiche.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CreateFicheComponent {
+  content: string = '';  // Variable pour stocker le contenu de l'éditeur
+  ficheName: string = ''; // Variable pour stocker le nom de la fiche
 
+  // Méthode pour enregistrer la fiche
+  saveFiche() {
+    console.log('Fiche sauvegardée avec le contenu : ', this.content);
+    // Ajouter ici le code pour enregistrer la fiche dans la base de données
+  }
+
+  // Méthode pour générer un PDF à partir du contenu de Quill
+  generatePDF() {
+    const element = document.querySelector('.ql-editor');
+    if (element) {
+      // Générer un PDF
+      html2pdf()
+        .from(element)
+        .save('fiche.pdf');
+    }
+  }
+   // Méthode pour gérer l'appui sur la touche "Entrée"
+   onEnterKey(event: KeyboardEvent): void {
+    event.preventDefault();
+    this.saveFiche(); // Enregistre la fiche uniquement
+  }
+  
 }
