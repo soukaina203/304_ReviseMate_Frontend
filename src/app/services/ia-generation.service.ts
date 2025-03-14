7d63e621ca6618eb67cb009e59766a0ddf29d9f4
samenthalo <ortega.samantha81400@gmail.com>
2025-03-14 18:48:45 +0100

    add quiz and flashcard

--- src/app/services/ia-generation.service.ts
+++ src/app/services/ia-generation.service.ts
@@ -1,4 +1,3 @@
-import { IA } from './../models/IA';
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from 'environments/environment';
@@ -12,11 +11,29 @@ export class IaGenerationService {
 
   constructor() {}
 
-  getIAanswerFromPdf(formData: FormData) {
-    return this.http.post(`${this.url}/revision/pdf`, formData);
+  getIAanswerFromPdf(formData: FormData, type: 'fiche' | 'carte' | 'quiz') {
+    const endpoint = type === 'fiche' 
+      ? 'revision/pdf' 
+      : type === 'carte' 
+      ? 'flashcard/pdf' 
+      : 'quizzes/pdf';  // Endpoint pour le quiz à partir d'un PDF
+    return this.http.post(`${this.url}/${endpoint}`, formData);
   }
 
-  getIAanswerFromText(textData: { text: string, customPrompt?: string }) {
-    return this.http.post(`${this.url}/revision`, textData);
+  getIAanswerFromText(textData: { text: string, customPrompt?: string }, type: 'fiche' | 'carte' | 'quiz') {
+    const endpoint = type === 'fiche' 
+      ? 'revision' 
+      : type === 'carte' 
+      ? 'flashcard' 
+      : 'quizzes';  // Endpoint pour le quiz à partir du texte
+    
+    // Adaptez le champ selon le type
+    const dataToSend = type === 'fiche' 
+      ? { text: textData.text, ...(textData.customPrompt && { customPrompt: textData.customPrompt }) }
+      : type === 'carte'
+      ? { content: textData.text, ...(textData.customPrompt && { customPrompt: textData.customPrompt }) }
+      : { content: textData.text };  // Données envoyées pour le quiz
+
+    return this.http.post(`${this.url}/${endpoint}`, dataToSend);
   }
 }
