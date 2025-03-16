e88c7155a05aaf8c529ca1e5164201fdedcbd415
samenthalo <ortega.samantha81400@gmail.com>
2025-03-17 00:30:40 +0100

    add files

--- src/app/services/uow.service.ts
+++ src/app/services/uow.service.ts
@@ -6,19 +6,32 @@ import { CarteService } from './carte.service';
 import { QuizService } from './quiz.service';
 import { AuthService } from './auth.service';
 import { IaGenerationService } from './ia-generation.service';
+import { QuizDataService } from './quiz-data.service'; // Importez le nouveau service
 
 @Injectable({
   providedIn: 'root'
 })
 export class UowService {
 
-  constructor() { }
-  users= new UserService();
-  classes= new ClasseService();
-  fiches= new FicheService();
-  cartes= new CarteService();
-  quiz= new QuizService();
-  auth= new AuthService();
-  ia= new IaGenerationService();
+  constructor(private quizDataService: QuizDataService) {
+    this.quiz = new QuizService();
+    this.users = new UserService();
+    this.classes = new ClasseService();
+    this.fiches = new FicheService();
+    this.cartes = new CarteService();
+    this.auth = new AuthService();
+    this.ia = new IaGenerationService();
+  }
 
+  users: UserService;
+  classes: ClasseService;
+  fiches: FicheService;
+  cartes: CarteService;
+  quiz: QuizService;
+  auth: AuthService;
+  ia: IaGenerationService;
+
+  getQuizQuestions(quizId: string) {
+    return this.quizDataService.getQuizQuestions(quizId);
+  }
 }
