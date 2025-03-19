6a991e2eecc44a72647bd6579edcdae9367ea71f
Soukaina Mourabit <soukainamourabit9@gmail.com>
2025-03-19 22:37:55 +0100

    changer les rediriction au niveau de login

--- src/app/modules/auth/sign-in/sign-in.component.ts
+++ src/app/modules/auth/sign-in/sign-in.component.ts
@@ -84,16 +84,8 @@ export class AuthSignInComponent implements OnInit {
                     if (res.message == "Connexion réussie") {
                         const userData = JSON.stringify(res.user);
                         localStorage.setItem('user', userData);
-                        let  role = res.user.id_role;
-                        if (role==="67c8621008049ddd39d069f1") {  // etudiant
                             this._router.navigateByUrl('/user');
-                        }
-                        if (role==="67bde3d6d528fe1ec83f0316") {  // prof
-                            this._router.navigateByUrl('/user');
-                        }
-                        if (role==="67c8621008049ddd39d069f2") {  // admin
-                            this._router.navigateByUrl('/admin');
-                        }
+
 
                     } else {
                         // Re-enable the form
